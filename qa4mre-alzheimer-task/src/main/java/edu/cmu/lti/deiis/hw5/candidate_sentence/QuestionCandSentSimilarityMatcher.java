ceef4d76f674ea01dba083d4b410bd05ab5b7c94
haibow <hbwang@gmail.com>
2013-11-25 01:06:49 -0500

    bug fix

--- qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/candidate_sentence/QuestionCandSentSimilarityMatcher.java
+++ qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/candidate_sentence/QuestionCandSentSimilarityMatcher.java
@@ -131,11 +131,11 @@ public class QuestionCandSentSimilarityMatcher  extends JCasAnnotator_ImplBase{
 		
 		for(int j=0;j<dependencies.size();j++){
 		  String rel = dependencies.get(j).getRelation();
-      String gov = dependencies.get(j).getGovernor().getText();
-      String dep = dependencies.get(j).getDependent().getText();
-      String depText = rel + "(" + gov + "," + dep + ")";
-      solrQuery+="dependencies:\""+depText+"\" ";
-    }
+	      String gov = dependencies.get(j).getGovernor().getText();
+	      String dep = dependencies.get(j).getDependent().getText();
+	      String depText = rel + "(" + gov + "," + dep + ")";
+	      solrQuery+="dependencies:\""+depText+"\" ";
+	    }
 		
 		solrQuery=solrQuery.trim();
 		
