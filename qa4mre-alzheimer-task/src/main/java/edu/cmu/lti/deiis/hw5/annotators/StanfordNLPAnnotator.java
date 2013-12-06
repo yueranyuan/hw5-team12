c1794f654f2a4c1c92b8166ad5b8f62fe6598f82
abhimanu <abhimanyu.kumar@gmail.com>
2013-12-05 22:35:33 -0500

    merge

--- qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/annotators/StanfordNLPAnnotator.java
+++ qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/annotators/StanfordNLPAnnotator.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
@@ -23,10 +24,14 @@ import edu.cmu.lti.qalab.types.Sentence;
 import edu.cmu.lti.qalab.types.TestDocument;
 import edu.cmu.lti.qalab.types.Token;
 import edu.cmu.lti.qalab.utils.Utils;
+import edu.stanford.nlp.dcoref.CorefChain;
+import edu.stanford.nlp.dcoref.CorefChain.CorefMention;
+import edu.stanford.nlp.dcoref.CorefCoreAnnotations.CorefChainAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
 import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.Label;
@@ -79,6 +84,10 @@ public class StanfordNLPAnnotator extends JCasAnnotator_ImplBase {
 				e.printStackTrace();
 				return;
 			}
+			
+			
+			
+			
 			List<CoreMap> sentences = document.get(SentencesAnnotation.class);
 			// SourceDocument sourcecDocument=(SourceDocument)
 			// jCas.getAnnotationIndex(SourceDocument.type);
@@ -94,6 +103,8 @@ public class StanfordNLPAnnotator extends JCasAnnotator_ImplBase {
 				Sentence annSentence = new Sentence(jCas);
 				ArrayList<Token> tokenList = new ArrayList<Token>();
 
+				
+				
 				// Dependency should have Token rather than String
 				for (int j = 0; j < sentence.get(TokensAnnotation.class).size(); j++) { // order
 																				// needs
